910678c5a06f06b62be6103e3091aae16c51fc4f
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-08-20 22:12:29 -0300

    Update OperadoresAritimeticos.java

--- Java-course/src/OperadoresAritimeticos.java
+++ Java-course/src/OperadoresAritimeticos.java
@@ -4,10 +4,13 @@ public class OperadoresAritimeticos {
         double placaVideo = 1000.00;
         double processador = 300.00;
         double placaMae = 600.00;
+        double desconto = 230.73;
 
         double valorPc= placaVideo + processador + placaMae;
+        double valorComDescontoPix = valorPc - desconto;
+        double valorTotalDividido= valorComDescontoPix / 2;
 
-        System.out.println( "Valor total do Pc = R$" + valorPc);
+        System.out.println( "Valor total do Pc = R$" + valorTotalDividido);
     }
 }
 
