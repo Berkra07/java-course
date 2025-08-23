b7aceaf4a7086c9827732f82ef00d53075b25127
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-08-22 22:39:51 -0300

    Create EntradaESaidaDeDados.java

---
+++ Java-course/src/EntradaESaidaDeDados.java
@@ -0,0 +1,16 @@
+public class EntradaESaidaDeDados {
+    public static void main(String[] args) {
+
+        /*Saidada de Dados
+        * Usamos System.out.print(), System.out,println() e System.out,printf() para imprimir dados na tela:
+        *
+        * System.out.print(): Imprime sem pular linha. (mais utilizado
+        * System.out,println(): Imprime e pula para prox. linha
+        * System.out,printf(): imprime formatado( Explicar melhor)*/
+
+        System.out.println("Impressão e pula linha");
+        System.out.print("Impressão e mesma linha");
+        System.out.printf("impressão formatada");
+        
+    }
+}
