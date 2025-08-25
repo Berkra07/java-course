589cd7a81a9668048d0620dee4828287e80b273d
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-08-25 12:15:36 -0300

    Create CastingConversaoTipos.java

---
+++ Java-course/src/CastingConversaoTipos.java
@@ -0,0 +1,23 @@
+public class CastingConversaoTipos {
+    public static void main(String[] args) {
+        /*  Casting (conversão de tipos)
+        *
+        * Conversão Implicita: Ocorre automaticamente quando convertemos um tipo menor para um tipo maior:
+        *       int i=10;
+        *       double d =i;
+        *
+        * Conversão Explícita: Precisamos fazer manualmente ao converter um tipo  maior para um tipo menor:
+        *       double d2= 10.5;
+        *       int i2= (int) d2;              */
+
+//       conversao Implicita:
+
+        int numero = 7;
+        double nd = numero;
+
+//       Conversão Explicita (Com casting)
+
+        double numero2 = 7.11;
+        int nd2 = (int) numero2;
+    }
+}
