94b79f4f0bd94b5b03eb02d9e0cf8ab42a457bdb
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-08-25 19:19:06 -0300

    Create EstruraCondicional.java

---
+++ Java-course/src/EstruraCondicional.java
@@ -0,0 +1,32 @@
+public class EstruraCondicional {
+    public static void main(String[] args) {
+
+        /*Estutura Condicional
+        *
+        * Esctrutura condicional permitem que seu programa tome decisões e
+        * execute diferentes blocos de código com base em certas condiçoes
+        *
+        * A estrutura condicional mais comum é o (if-else)
+        *       if -> Se.
+        *       else -> Senão.
+        *
+        * Expressoes comparativas
+        *
+        * Expressoes que comparam dois valores e retornam um valor booleano (true or false)
+        *
+        * Operadores comparativos:
+        *
+        * == : igual a
+        * != : diferente
+        * > : maior que
+        * < : menor que
+        * >= : maior ou igual a
+        * <= : menor ou igual a           */
+
+        int idade1 = 18;
+        int idade2 = 16;
+
+        System.out.println( idade1 == idade2);
+        
+    }
+}
