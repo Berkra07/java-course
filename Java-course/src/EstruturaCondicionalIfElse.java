6855ce53ea63cffc16844fdad5aa517b782a5ba9
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-08-27 19:28:34 -0300

    Update EstruturaCondicionalIfElse.java

--- Java-course/src/EstruturaCondicionalIfElse.java
+++ Java-course/src/EstruturaCondicionalIfElse.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class EstruturaCondicionalIfElse {
     public static void main(String[] args) {
+
         /* Estrutura condicional (If-Else)
         *
         * if(condição) {
@@ -10,25 +11,31 @@ public class EstruturaCondicionalIfElse {
         * } (senão execute)
          * If-> se-> Se condição for verdadeira execute ...
          * Elsee-> senão-> se a condição for falsa execute...  */
-//       ex 1
-        int idade =18;
+
+//       ex 1 ----------------------------------------------------------------------------------------------------
+
+        /*int idade =18;
 
         if (idade >=18){
             System.out.println("Você é maior de idade!");
         }
         else {
             System.out.println("Você é menor de idade");
-        }
-//       ex 2
-        boolean chovendo = false;
+        }*/
+
+//       ex 2 ----------------------------------------------------------------------------------------------------
+
+        /*boolean chovendo = false;
         if(!chovendo){
             System.out.println("Vamos correr");
         }
         else {
             System.out.println("Vamos lêr");
-        }
-//      ex 3
-        boolean hmoney = false;
+        }*/
+
+//      ex 3 ----------------------------------------------------------------------------------------------------
+
+        /*boolean hmoney = false;
         boolean hlimit = false;
 
         if (hmoney && hlimit){
@@ -39,19 +46,20 @@ public class EstruturaCondicionalIfElse {
         }
         else {
             System.out.println("faz algo caseiro.");
-        }
+        }*/
 
-//        Ex 4
-
+//        Ex 4 ----------------------------------------------------------------------------------------------------
+/*
         int numero = -1;
 
         if (numero >= 0){
             System.out.println("numero positivo");
         }
         else {
-            System.out.println("numero negativo");
-//      Ex 5
-            Scanner sc = new Scanner(System.in);
+            System.out.println("numero negativo");*/
+//      Ex 5 w
+
+           /* Scanner sc = new Scanner(System.in);
 
             // Lê o número digitado pelo usuário
             System.out.print("Digite um número inteiro: ");
@@ -63,9 +71,10 @@ public class EstruturaCondicionalIfElse {
                 System.out.println("O número " + numero1 + " é PAR.");
             } else {
                 System.out.println("O número " + numero1 + " é ÍMPAR.");
-            }
-//
-            
+            }*/
+
+//      Ex 6
+        
         }
     }
 }
