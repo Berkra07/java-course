c1fa5b4dced2d1e1266b79225a742108109215fb
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-08-26 22:12:40 -0300

    Create OperadoresLogicos.java

---
+++ Java-course/src/OperadoresLogicos.java
@@ -0,0 +1,14 @@
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+
+        /* Operadores Lógicos
+        *
+        * && : e lógico (AND)
+        * || : ou lógico (OR)
+        * !: Negação lógica (NOT)*/
+
+
+        boolean x = true;
+        boolean y = false;
+    }
+}
