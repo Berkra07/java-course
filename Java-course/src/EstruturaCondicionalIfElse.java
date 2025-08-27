6ab740f695ecaec2d25313a70d166fae71c566b2
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-08-26 22:28:17 -0300

    Update EstruturaCondicionalIfElse.java

--- Java-course/src/EstruturaCondicionalIfElse.java
+++ Java-course/src/EstruturaCondicionalIfElse.java
@@ -14,6 +14,9 @@ public class EstruturaCondicionalIfElse {
         if (idade >=18){
             System.out.println("Você é maior de idade!");
         }
+        else {
+            System.out.println("Você é menor de idade");
+        }
 
 
 
