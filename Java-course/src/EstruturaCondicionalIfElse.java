fee84c9e0cf3602cee5eb982980fd7bbc3c9e111
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-08-27 20:04:46 -0300

    Update EstruturaCondicionalIfElse.java

--- Java-course/src/EstruturaCondicionalIfElse.java
+++ Java-course/src/EstruturaCondicionalIfElse.java
@@ -75,17 +75,6 @@ public class EstruturaCondicionalIfElse {
 
 //      Ex 6
 
-        Scanner nota = new Scanner(System.in);
 
-        System.out.println("Digite sua nota: ");
-        int notaSchool = nota.nextInt();
-
-        if (notaSchool >= 70) {
-            System.out.println("Você tirou " + notaSchool + ". Aprovado.");
-        } else if (notaSchool >= 60 && notaSchool < 70) {
-            System.out.println("Você tirou " + notaSchool + ".Recuperação.");
-        } else {
-            System.out.println("reprovado");
-        }
     }
 }
