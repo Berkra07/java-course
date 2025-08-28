2cbeb298836c5717bd0fa4ec767d4aeba04c96a1
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-08-28 19:30:32 -0300

    Create ClassificacaoIdade.java

---
+++ Java-course/src/JavachallengesIfElse/ClassificacaoIdade.java
@@ -0,0 +1,14 @@
+package JavachallengesIfElse;
+
+import java.util.Scanner;
+
+public class ClassificacaoIdade {
+    public static void main(String[] args) {
+
+        Scanner scIdade = new Scanner(System.in);
+
+        System.out.println("Digite sua idade: ");
+        int idade = scIdade.nextInt();
+
+    }
+}
