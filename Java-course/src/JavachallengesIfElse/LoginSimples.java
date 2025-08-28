4193f47fce17ca3e9ba0567527dae869c748cfed
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-08-28 20:02:43 -0300

    Update LoginSimples.java

--- Java-course/src/JavachallengesIfElse/LoginSimples.java
+++ Java-course/src/JavachallengesIfElse/LoginSimples.java
@@ -9,16 +9,14 @@ public class LoginSimples {
         Scanner scSenha = new Scanner(System.in);
 
         System.out.println("Digite seu Login: ");
-        System.out.println("Digite sua senha: ");
-
         String login = scLogin.nextLine();
+
+        System.out.println("Digite sua senha: ");
         int senha = scSenha.nextInt();
 
-        if (senha== 1234 && login.equals("admin") ){
-            System.out.println("bem vindo Bernardo!");
-        }
-        else {
-            System.out.println("Login invalido");
+
+
+        
         }
     }
 }
