5aef4b6de155a04799d423d93ca5963345679f34
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-08-28 20:09:42 -0300

    Create DesafiosSwitch.java

---
+++ Java-course/src/JavachallengesIfElse/DesafiosSwitch.java
@@ -0,0 +1,15 @@
+package JavachallengesIfElse;
+
+import java.util.Scanner;
+
+public class DesafiosSwitch {
+    public static void main(String[] args) {
+
+
+//        DESAFIO 01: Dia da semana --------------------------------------------------------------
+
+        Scanner scWeekVerific = new Scanner(System.in);
+
+        System.out.println();
+    }
+}
