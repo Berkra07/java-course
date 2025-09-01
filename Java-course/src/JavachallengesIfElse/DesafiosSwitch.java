de9ff625ebb94f31eac71b2e6c42bf59161ad103
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-08-31 21:53:28 -0300

    Update DesafiosSwitch.java

--- Java-course/src/JavachallengesIfElse/DesafiosSwitch.java
+++ Java-course/src/JavachallengesIfElse/DesafiosSwitch.java
@@ -18,6 +18,24 @@ public class DesafiosSwitch {
 
             case 1:
                 System.out.println("Hojé é Segunda-feira");
+                break;
+            case 2:
+                System.out.println("Hoje é terça-feira");
+            break;
+            case 3:
+                System.out.println("Hoje é quarta-feira");
+                break;
+            case 4:
+                System.out.println("hoje é quinta-feira");
+                break;
+            case 5:
+                System.out.println("Hoje é Sexta-feira");
+                break;
+            case 6:
+                System.out.println("Hoje é Sabado");
+                break;
+            case 7:
+                System.out.println("Hoje é Domingo");
         }
     }
 }
