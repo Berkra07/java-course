f6d180865f59af60c764c1d6534ee0b960d186b8
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-09-02 20:37:24 -0300

    Create EstuturasRepetitivas.java

---
+++ Java-course/src/EstuturasRepetitivas.java
@@ -0,0 +1,18 @@
+public class EstuturasRepetitivas {
+    public static void main(String[] args) {
+/*                   ESTRUTURAS REPETITIVAS:
+*
+*      Estruturas repetitivas permitem que um bloco de codigo seja executado varias vezes, enquanto uma condição especifica
+*      for verdadeira.
+*
+*       Principais estruturas repetitivas em Java: While, For e do-while.
+*
+*             Estruturas While
+*       a estrutura while executa um bloco de codigo repetidamente enquanto a condiçao especificada for verdadeira
+*       Ele verifica a condição no início
+    */
+
+
+
+    }
+}
