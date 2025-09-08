b075169f127d12ddd6b646f01d1e35a8184da43c
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-09-08 20:08:09 -0300

    Revert "Merge pull request #1 from Berkra07/feature-login"
    This reverts commit 1a2833460cdadbbb63e41311e87238e97fe7acbc, reversing
    changes made to c922c92e6a5cf28bbb65bc2f8378feefa64f0e19.

--- Java-course/src/JavachallengesIfElse/DesafiosSwitch.java
+++ Java-course/src/JavachallengesIfElse/DesafiosSwitch.java
@@ -117,7 +117,7 @@ class desafioQuatro {
                 System.out.println("Vôce escolheu Euro.");
                 break;
             case 3:
-                System.out.println("Você escolheu Real Brasileiro (R$).");
+                System.out.println("Você escolheu Real Brasileiro.");
         }
     }
 }
