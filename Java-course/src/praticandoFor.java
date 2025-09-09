bf38534f24942fc1df249a824d10bc3c8a6499e2
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-09-09 20:19:28 -0300

    Create praticandoFor.java

---
+++ Java-course/src/praticandoFor.java
@@ -0,0 +1,8 @@
+public class praticandoFor {
+    public static void main(String[] args) {
+
+        for (int numr= 0; numr<= 20; numr +=2 ){
+            System.out.println("numero pares: " + numr);
+        }
+    }
+}
