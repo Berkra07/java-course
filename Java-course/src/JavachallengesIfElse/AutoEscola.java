d1b4c6c49655a0b046e6b266dc82e4ae84449306
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-09-12 20:16:08 -0300

    Create AutoEscola.java

---
+++ Java-course/src/JavachallengesIfElse/AutoEscola.java
@@ -0,0 +1,28 @@
+package JavachallengesIfElse;
+
+import java.util.Scanner;
+
+public class AutoEscola {
+    public static void main(String[] args) {
+
+        Scanner ScQuestions = new Scanner(System.in);
+
+       int pontos = 0;
+       boolean desclassificado = false;
+
+           System.out.println("Simulado prova do detran \uD83D\uDEA6");
+
+           System.out.println("Você queimou faixa: ");
+           String resposta1 = ScQuestions.nextLine();
+
+           if (resposta1.equalsIgnoreCase("sim")) {
+               pontos += 3;
+           }
+
+           System.out.println("Você afogou a moto:");
+
+
+    }
+}
+
+
