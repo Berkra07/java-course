e775037d1e9748582df44b2bbb7a552f205c051e
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-09-12 19:49:44 -0300

    Update AprendendoArrays.java

--- Java-course/src/AprendendoArrays.java
+++ Java-course/src/AprendendoArrays.java
@@ -4,6 +4,18 @@ public class AprendendoArrays {
         String[] guns = {"Ak-47","M4-A4","AWM"};
 
         System.out.println(guns[0]); // Os elementos da Array sempre começa em 0
-        
+
+
+//      Outra forma de usar o Array:
+
+        String [] cars = new String[5];
+
+        cars [0] = "Polo";
+        cars [1] = "Gol";
+        cars [2] = "Virtus";
+        cars [3] = "Ferrari";
+        cars [4] = "Mustang";
+
+        System.out.println("O seu " + cars[4] + " está na garagem N: 47");
     }
 }
