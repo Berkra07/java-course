3b545085283bfacc2cad0c4f6066b8274fd56e2b
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-09-13 18:53:41 -0300

    Fix calculation of banknote and coin denominations
    Updated the logic to correctly update the remainder after each denomination is processed, ensuring accurate output for all banknote and coin values. Also fixed a typo in the output for 1 real coins.

--- Java-course/src/JavachallengesIfElse/CaixaEletronico.java
+++ Java-course/src/JavachallengesIfElse/CaixaEletronico.java
@@ -8,33 +8,38 @@ public class CaixaEletronico {
 
         Scanner scValorTotal = new Scanner(System.in);
         int valorTotal = scValorTotal.nextInt();
-        int resto = valorTotal % 50;
+        int resto = valorTotal % 50 ;
+
         if (valorTotal>=50) {
             int ceulasDe50 = valorTotal / 50;
             System.out.println("Cedulas de 50= " + ceulasDe50);
+            resto = resto % 50;
         }
 
         if (resto>=20) {
         int cedulasDe20 = resto / 20;
         System.out.println("cedulas de 20= " + cedulasDe20);
+            resto = resto % 20;
         }
 
         if (resto>= 10){
             int cedulasDe10 = resto / 10;
             System.out.println("Cédulas de R$ 10,00 = " + cedulasDe10);
+            resto = resto % 10;
         }
         if (resto>= 5){
             int cedulasDe5 = resto / 5;
             System.out.println("Cédulas de R$ 5,00= " + cedulasDe5);
+            resto = resto % 5;
         }
         if (resto>= 2){
             int cedulasDe2 = resto / 2;
             System.out.println("Cedulas de R$ 2,00 = " + cedulasDe2);
+            resto = resto % 2;
         }
         if (resto>=1){
             int moedaDe1 = resto;
-            System.out.println("Moedas de R$ 1,00 = s" + moedaDe1);
-
+            System.out.println("Moedas de R$ 1,00 =" + moedaDe1);
         }
     }
 }
