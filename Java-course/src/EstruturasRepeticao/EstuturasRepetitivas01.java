2c2ee3d71130b4dfa780d2ac7c1f2d367e8680ad
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-09-17 19:46:33 -0300

    Add example of while loop in Java
    Introduces EstuturasRepetitivas01.java demonstrating the use of a while loop to print numbers from 1 to 20, with comments explaining repetitive structures in Java.

---
+++ Java-course/src/EstruturasRepeticao/EstuturasRepetitivas01.java
@@ -0,0 +1,24 @@
+package EstruturasRepeticao;
+
+public class EstuturasRepetitivas01 {
+    public static void main(String[] args) {
+/*                   ESTRUTURAS REPETITIVAS:
+*
+*      Estruturas repetitivas permitem que um bloco de codigo seja executado varias vezes, enquanto uma condição especifica
+*      for verdadeira.
+*
+*       Principais estruturas repetitivas em Java: While, For e do-while.
+*
+*             Estruturas While
+*       a estrutura while executa um bloco de codigo repetidamente enquanto a condiçao especificada for verdadeira
+*       Ele verifica a condição no início
+    */
+
+            int numero = 1;
+
+            while (numero <= 20){
+            System.out.println("numero : " + numero);
+            numero ++;
+        }
+    }
+}
