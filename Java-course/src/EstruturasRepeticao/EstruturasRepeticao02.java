151f6da0f4f4ca146bf3f025c8008fd86f092f35
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-09-17 19:49:46 -0300

    Create EstruturasRepeticao02.java

---
+++ Java-course/src/EstruturasRepeticao/EstruturasRepeticao02.java
@@ -0,0 +1,13 @@
+package EstruturasRepeticao;
+
+public class EstruturasRepeticao02 {
+    public static void main(String[] args) {
+        //Utilizando while
+
+        int cambalhotas = 1;
+         while (cambalhotas<=4){
+             System.out.println("Cabalhotas = "cambalhotas);
+             cambalhotas ++;
+         }
+    }
+}
