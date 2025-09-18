92ed34cd7dcaa16f0a7181bc9eabd285e8a43ff9
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-09-18 07:21:43 -0300

    Add example of for loop structure in Java
    Introduces EstruturaRepeticao04.java demonstrating the use of a for loop to print values from 0 to 4. This serves as an educational example for repetition structures.

---
+++ Java-course/src/EstruturasRepeticao/EstruturaRepeticao04.java
@@ -0,0 +1,15 @@
+package EstruturasRepeticao;
+
+public class EstruturaRepeticao04 {
+    public static void main(String[] args) {
+
+        /*Estrutura - FOR*/
+        // A estrutura for é usada para repetir um bloco de código um número especifico de vezes
+
+        int cont = 0;
+
+        for (; cont<5;cont++){
+            System.out.println("valor : " + cont);
+        }
+    }
+}
