e4c788d740777ca6d254f05d3037e2e78995615e
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-09-19 23:08:36 -0300

    Update EstruturaRepeticao06.java

--- Java-course/src/EstruturasRepeticao/EstruturaRepeticao06.java
+++ Java-course/src/EstruturasRepeticao/EstruturaRepeticao06.java
@@ -5,14 +5,13 @@ import java.util.Scanner;
 public class EstruturaRepeticao06 {
     public static void main(String[] args) {
 
-        System.out.println("Digite um numero positivo e tente acerta o numero certo");
-
         Scanner Scnumero = new Scanner(System.in);
         int numero;
 
         do {
-
-        }
-
+            System.out.println("Digite um valor positivo");
+            numero = Scnumero.nextInt();
+        }while (numero <= 0);
+        System.out.println("Você digitou: " + numero);
     }
 }
