cbb9d42aa1713c73039c89a14cf15a09473e2d20
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-09-19 23:20:39 -0300

    Refactor for loop increment syntax
    Updated the for loop increment from 'numero= numero+2' to 'numero += 2' for improved readability and consistency with Java coding conventions.

--- Java-course/src/EstruturasRepeticao/EstruturaRepeticao07.java
+++ Java-course/src/EstruturasRepeticao/EstruturaRepeticao07.java
@@ -3,7 +3,7 @@ package EstruturasRepeticao;
 public class EstruturaRepeticao07 {
     public static void main(String[] args) {
 
-        for (int numero = 0; numero <= 20; numero= numero+2) {
+        for (int numero = 0; numero <= 20; numero += 2) {
             System.out.println(numero);
         }
     }
