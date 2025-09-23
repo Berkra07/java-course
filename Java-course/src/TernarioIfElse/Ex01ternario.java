951bcc5b581b145f37257db4a8ce4ad63713f3da
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-09-22 21:26:07 -0300

    Add example using ternary operator for age check
    Introduces Ex01ternario.java demonstrating the use of the ternary operator to determine if a person is of legal age based on the 'idade' variable.

---
+++ Java-course/src/TernarioIfElse/Ex01ternario.java
@@ -0,0 +1,11 @@
+package TernarioIfElse;
+
+public class Ex01ternario {
+    public static void main(String[] args) {
+
+        int idade = 18;
+
+        String resultado = (idade > 17) ? "você é maior de idade" : "Menor de idade";
+        System.out.println( resultado);
+    }
+}
