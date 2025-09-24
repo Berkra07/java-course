44c319142cbe5b671932299af0011293b50b4c64
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-09-23 21:19:09 -0300

    Reformat Caneta class and add status method
    Reorganized the Caneta class by adjusting indentation and adding a placeholder status() method. No functional changes were made; methods remain empty.

--- POO-course/src/aula02/Aula02.java
+++ POO-course/src/aula02/Aula02.java
@@ -7,5 +7,6 @@ public class Aula02 {
         c1.cor= "Azul";
         c1.ponta= 0.5f;
         c1.tampada = false;
+        c1.status();
     }
 }
--- POO-course/src/aula02/Caneta.java
+++ POO-course/src/aula02/Caneta.java
@@ -1,22 +1,24 @@
 package aula02;
 
 public class Caneta {
-    String modelo;
-    String cor;
-    float ponta;
-    int carga;
-    boolean tampada;
 
-    void rabiscar(){
 
-    }
+        String modelo;
+        String cor;
+        float ponta;
+        int carga;
+        boolean tampada;
 
-    void tampar (){
+        void status(){
+            System.out.print("Uma caneta " + this.cor);
+            System.out.println("esta tampada? " + this.tampada );
+        }
+        void rabiscar(){
 
-    }
+        }
+        void tampar(){
 
-    void destampar() {
-
-    }
-
+        }
+        void destampar (){
+        }
 }
