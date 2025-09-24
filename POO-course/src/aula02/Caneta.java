90267f4265ab0580f2c0ba0e5aed9bcea0a9591f
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-09-24 20:20:03 -0300

    Update access modifiers in Caneta class
    Changed the access modifiers of several fields and methods in the Caneta class to better encapsulate data and control visibility. 'ponta' is now private, 'carga' and 'tampada' are protected, and methods have been updated to public or protected as appropriate.

--- POO-course/src/aula02/Caneta.java
+++ POO-course/src/aula02/Caneta.java
@@ -5,28 +5,28 @@ public class Caneta {
 
         public String modelo;
         public String cor;
-        float ponta;
-        float carga;
-        boolean tampada;
+        private float ponta;
+        protected float carga;
+        protected boolean tampada;
 
-        void status(){
+        public void status(){
             System.out.println("Modelo: " + this.modelo);
             System.out.println("Cor: " + this.cor);
             System.out.println("Ponta: " + this.ponta);
             System.out.println("Carga: " + this.carga);
             System.out.println("tampada? " + this.tampada );
         }
-        void rabiscar(){
+        public void rabiscar(){
             if (this.tampada ){
                 System.out.println("ERRO! A caneta esta tampada");
             } else {
                 System.out.println("Rabiscando...");
             }
         }
-        void tampar(){
+        protected void tampar(){
             this.tampada=true;
         }
-        void destampar (){
+        protected void destampar (){
             this.tampada=false;
         }
 }
