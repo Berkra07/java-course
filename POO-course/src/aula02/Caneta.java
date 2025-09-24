398d8f968ae9faa96da12cddf938182c83e41fbe
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-09-23 21:26:03 -0300

    Add modelo and update carga type in Caneta class
    Introduced the 'modelo' attribute and changed 'carga' from int to float in the Caneta class. Updated the status method to display modelo, ponta, and carga. Adjusted Aula02 to set modelo and carga accordingly.

--- POO-course/src/aula02/Caneta.java
+++ POO-course/src/aula02/Caneta.java
@@ -6,11 +6,14 @@ public class Caneta {
         String modelo;
         String cor;
         float ponta;
-        int carga;
+        float carga;
         boolean tampada;
 
         void status(){
-            System.out.print("Uma caneta " + this.cor);
+            System.out.println("Modelo: " + this.modelo);
+            System.out.print("cor: " + this.cor);
+            System.out.println("Ponta: " + this.ponta);
+            System.out.println("Carga: " + this.carga);
             System.out.println("esta tampada? " + this.tampada );
         }
         void rabiscar(){
