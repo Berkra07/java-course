a72c85df237de9b789cffbbbb74c2699a23e57ce
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-09-24 20:28:20 -0300

    Set Caneta attributes before calling status
    Added assignments for modelo, cor, and carga properties of the Caneta object before invoking the status method in Aula02.java.

--- POO-course/src/aula02/Aula02.java
+++ POO-course/src/aula02/Aula02.java
@@ -4,6 +4,9 @@ public class Aula02 {
     public static void main(String[] args) {
 
         Caneta c1 = new Caneta();
+        c1.modelo = "Bic prime";
+        c1.cor = "Preto";
+        c1.carga = 80;
         c1.status();
     }
 }
