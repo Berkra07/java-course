97d658f3eab7e371cefcf4f4ebc6fb5324ac4aa7
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-09-29 20:53:17 -0300

    Add color and cap state to CanetaAula04 class
    Introduced 'cor' and 'tampada' attributes to CanetaAula04, with default color set to 'Azul' and the pen starting capped. Added methods to cap and uncap the pen, and updated the status method to display the new attributes.

--- POO-course/src/aula004/CanetaAula04.java
+++ POO-course/src/aula004/CanetaAula04.java
@@ -3,6 +3,13 @@ package aula004;
 public class CanetaAula04 {
     public String modelo;
     private Float ponta;
+    private boolean tampada;
+    private String cor;
+
+    public CanetaAula04(){
+        this.tampar();
+        this.cor = "Azul";
+    }
 
     public String getModelo(){
         return this.modelo;
@@ -20,9 +27,18 @@ public class CanetaAula04 {
         this.ponta = p;
     }
 
+    public void tampar(){
+        this.tampada = true;
+    }
+    public void destampar (){
+        this.tampada = false;
+    }
+
     public void status (){
         System.out.println("Sobre a caneta:");
         System.out.println("Modelo: " + this.getModelo());
         System.out.println("Ponta: " + this.getPonta());
+        System.out.println( "Cor: "+ cor );
+        System.out.println("tampada: " + tampada);
     }
 }
