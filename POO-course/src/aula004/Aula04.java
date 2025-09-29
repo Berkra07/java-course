9e3e68784cfdeddc599914c30ffb3c5a8ed385b0
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-09-29 20:43:36 -0300

    Add example usage of getters in Aula04.java
    Commented out the status() method call and added a print statement demonstrating the use of getModelo() and getPonta() getters for the CanetaAula04 object.

--- POO-course/src/aula004/Aula04.java
+++ POO-course/src/aula004/Aula04.java
@@ -7,6 +7,14 @@ public class Aula04 {
         CanetaAula04 caneta1 = new CanetaAula04();
         caneta1.setModelo("Bic");
         caneta1.setPonta(0.5f);
-        caneta1.status();
+       // caneta1.status();
+
+
+        // Getters
+        System.out.println("Eu tenho uma Caneta " + caneta1.getModelo() + " de ponta " + caneta1.getPonta());
+
+
+        // Metodo construtor
+
     }
 }
