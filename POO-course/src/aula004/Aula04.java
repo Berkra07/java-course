a5e3d96b487c3ecc3f957c209158148dcdab8a7a
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-09-29 20:53:13 -0300

    Update CanetaAula04 usage in Aula04.java
    Changed the model to 'Bic ROxo' and the tip size to 0.9f for caneta1. Re-enabled the status() method call and removed commented-out code and print statements for cleaner output.

--- POO-course/src/aula004/Aula04.java
+++ POO-course/src/aula004/Aula04.java
@@ -5,16 +5,9 @@ import aula02.Caneta;
 public class Aula04 {
     public static void main(String[] args) {
         CanetaAula04 caneta1 = new CanetaAula04();
-        caneta1.setModelo("Bic");
-        caneta1.setPonta(0.5f);
-       // caneta1.status();
-
-
-        // Getters
-        System.out.println("Eu tenho uma Caneta " + caneta1.getModelo() + " de ponta " + caneta1.getPonta());
-
-
-        // Metodo construtor
+        caneta1.setModelo("Bic ROxo");
+        caneta1.setPonta(0.9f);
+        caneta1.status();
 
     }
 }
