c5b8a90fcd4407fe63fb670d2969257736f0d803
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-09-29 21:12:57 -0300

    Add second pen instance in Aula04 main method
    Introduced a new CanetaAula04 object (caneta2) with different attributes and displayed its status, providing an additional example in the main method.

--- POO-course/src/aula004/Aula04.java
+++ POO-course/src/aula004/Aula04.java
@@ -6,6 +6,9 @@ public class Aula04 {
     public static void main(String[] args) {
         CanetaAula04 caneta1 = new CanetaAula04("Chulebrão","Rosa", 0.9f);
         caneta1.status();
-
+        System.out.println("-------------------------------------");
+        System.out.println("Caneta Dois");
+        CanetaAula04 caneta2 = new CanetaAula04("Chureba", "Marrom", 1.0f);
+        caneta2.status();
     }
 }
