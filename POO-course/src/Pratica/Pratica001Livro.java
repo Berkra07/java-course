bf6fc6dcd6c914f8180f41760cdbfd6cdd190dc9
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-09-30 20:49:43 -0300

    Add capitulos field and nome getter/setter to Livro
    Introduced a new private 'capitulos' field to the Pratica001Livro class. Added getter and setter methods for the 'nome' property to improve encapsulation.

--- POO-course/src/Pratica/Pratica001Livro.java
+++ POO-course/src/Pratica/Pratica001Livro.java
@@ -4,6 +4,15 @@ public class Pratica001Livro {
     public String nome;
     public String autor;
     private int paginas;
-    
+    private int capitulos;
 
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String n){
+        this.nome=n;
+    }
+    
 }
