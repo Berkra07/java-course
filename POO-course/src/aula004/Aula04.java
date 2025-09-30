84aca605a0e60834dab02f2d9071e43db0f342af
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-09-29 21:05:08 -0300

    Update CanetaAula04 instantiation with constructor
    Replaces the default constructor and setter calls for CanetaAula04 with a parameterized constructor, passing model, color, and tip size directly. This streamlines object creation and initialization.

--- POO-course/src/aula004/Aula04.java
+++ POO-course/src/aula004/Aula04.java
@@ -4,9 +4,7 @@ import aula02.Caneta;
 
 public class Aula04 {
     public static void main(String[] args) {
-        CanetaAula04 caneta1 = new CanetaAula04();
-        caneta1.setModelo("Bic ROxo");
-        caneta1.setPonta(0.9f);
+        CanetaAula04 caneta1 = new CanetaAula04("Chulebrão","Rosa", 0.9f);
         caneta1.status();
 
     }
