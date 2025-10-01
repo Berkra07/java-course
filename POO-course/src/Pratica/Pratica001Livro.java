ba0a7d03f4d3f8cf6b43e261fcf6cfe670e3d097
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-10-01 13:22:14 -0300

    Adicionar atributos, getters/setters e método status
    update classe Livro: adicionar atributos, getters/setters e método status completo

--- POO-course/src/Pratica/Pratica001Livro.java
+++ POO-course/src/Pratica/Pratica001Livro.java
@@ -1,47 +1,23 @@
 package Pratica;
 
 public class Pratica001Livro {
-    public String nome;
+    private String nome;
     public String autor;
     public int paginas;
     public int capitulos;
 
 
-    public String getNome(){
-        return this.nome;
+    public String getNome() {
+        return nome;
     }
 
-    public void setNome(String n){
-        this.nome=n;
-    }
-
-    public String getAutor(){
-        return this.autor;
-    }
-
-    public void setAutor(String a){
-        this.autor=a;
-    }
-
-    public int getPaginas(){
-        return this.paginas;
-    }
-
-    public void setPaginas(int p){
-        this.paginas=p;
-    }
-
-    public int getCapitulos(){
-        return this.capitulos;
-    }
-
-    public void setCapitulos(int c){
-        this.capitulos=c;
+    public void setNome(String n) {
+        this.nome = n;
     }
 
     public void status (){
         System.out.println("Sobre o livro: ");
-        System.out.println("Nome do  Livro: " + this.getNome());
+        System.out.println("Nome do  Livro: " + getNome());
         System.out.println("Nome do Autor: " + autor);
         System.out.println("Número de páginas: " + paginas);
         System.out.println("Número de capitulos: " + capitulos );
