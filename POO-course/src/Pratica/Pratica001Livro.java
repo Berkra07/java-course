affb3d25bbe2f086a6ba6fbfdd9f95e06dd7852e
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-09-30 21:05:36 -0300

    Update Pratica001Livro.java

--- POO-course/src/Pratica/Pratica001Livro.java
+++ POO-course/src/Pratica/Pratica001Livro.java
@@ -30,4 +30,20 @@ public class Pratica001Livro {
     public void setPaginas(int p){
         this.paginas=p;
     }
+
+    public int getCapitulos(){
+        return this.capitulos;
+    }
+
+    public void setCapitulos(int c){
+        this.capitulos=c;
+    }
+
+    public void status (){
+        System.out.println("Sobre o livro: ");
+        System.out.println("Nome do  Livro: " + nome);
+        System.out.println("Nome do Autor: " + autor);
+        System.out.println("Número de páginas: " + paginas);
+        System.out.println("Número de capitulos: " + autor );
+    }
 }
