783cd7078e8bfa089f6e3be5cd0421f0dc6bdf90
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-10-07 21:12:20 -0300

    Create BancoCLiente.java

---
+++ POO-course/src/Pratica/pratica02/BancoCLiente.java
@@ -0,0 +1,10 @@
+package Pratica.pratica02;
+
+public class BancoCLiente {
+    public static void main(String[] args) {
+
+        BancoCLiente cLiente01 = new BancoCLiente();
+        
+
+    }
+}
