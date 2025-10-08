edf8b046b07a5549e1cb07c2d867ccdbb7a7e2e0
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-10-07 21:35:16 -0300

    Update BancoCLiente.java

--- POO-course/src/Pratica/pratica02/BancoCLiente.java
+++ POO-course/src/Pratica/pratica02/BancoCLiente.java
@@ -2,9 +2,9 @@ package Pratica.pratica02;
 
 public class BancoCLiente {
     public static void main(String[] args) {
+        ContaBanco cLiente01 = new ContaBanco();
+        cLiente01.numConta = 10;
 
-        BancoCLiente cLiente01 = new BancoCLiente();
-        
 
     }
 }
