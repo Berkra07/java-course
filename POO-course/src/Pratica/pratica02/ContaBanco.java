0db6eab05f602e2f360cef615dae327b69332fc0
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-10-08 19:44:54 -0300

    Update ContaBanco.java

--- POO-course/src/Pratica/pratica02/ContaBanco.java
+++ POO-course/src/Pratica/pratica02/ContaBanco.java
@@ -2,18 +2,39 @@ package Pratica.pratica02;
 
 public class ContaBanco {
 
-    int numConta;
-    String tipo;
-    String proprietario;
-    float saldo;
-    boolean abrirConta;
+    public int numConta;
+    protected String tipo;
+    private String proprietario;
+    private float saldo;
+    private boolean abrirConta;
 
+    public void abrirConta(){
 
+    }
+    public void fecharConta(){
+
+    }
+    public void depositar(){
+
+    }
+    public void sacar(){
+
+    }
+    public void pagarMensalidade(){
+
+    }
+
+    //Métodos Especiais
+    public void ContaBanco(){
+
+    }
+    public void setNumConta(int n){
+        this.numConta = n;
+    }
+    public int getNumConta(){
+        return this.numConta;
+    }
     public void status(){
-        System.out.println("numero da conta: " + numConta);
-        System.out.println("Tipo de conta:  "+tipo);
-        System.out.println("Proprietario da conta: " + proprietario);
-        System.out.println("Saldo da conta: R$" + saldo);
-        System.out.println(abrirConta);
+
     }
 }
