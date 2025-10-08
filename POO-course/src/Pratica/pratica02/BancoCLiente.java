6af56193a951b7d2744d732d4fb1b39f47a13ca9
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-10-08 12:12:49 -0300

    Update BancoCLiente.java

--- POO-course/src/Pratica/pratica02/BancoCLiente.java
+++ POO-course/src/Pratica/pratica02/BancoCLiente.java
@@ -5,8 +5,8 @@ public class BancoCLiente {
         ContaBanco cLiente01 = new ContaBanco();
         cLiente01.numConta = 10;
         cLiente01.tipo = "Corrente";
-        cLiente01.dono="bernardo";
-        cLiente01.saldo= 99.00f;
+        cLiente01.proprietario ="bernardo";
+        cLiente01.saldo= 99.55f;
         cLiente01.abrirConta=true;
 
         cLiente01.status();
