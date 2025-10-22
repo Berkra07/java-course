f09a57d100e9da05e54f2e158771685b0d37ddc1
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-10-21 21:24:45 -0300

    Update Pratica.java

--- Java-course/src/Pratica.java
+++ Java-course/src/Pratica.java
@@ -5,11 +5,11 @@ public class Pratica {
 
         Scanner ScNumeros = new Scanner(System.in);
 
-        System.out.println("Digite um numero");
-        int Numero= ScNumeros.nextInt();
+        System.out.println("Digite um numero para saber se ele è par ou impar");
+        int numero= ScNumeros.nextInt();
 
-        if (Numero %  2==0){
-            System.out.println("NUmero par");
+        if (numero %  2==0){
+            System.out.println("O numero " + numero + " é par");
         }
         else {
             System.out.println("Numero impar");
