ec77d6d6ac047e6343213efb278053a460e421e5
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-10-21 21:17:51 -0300

    Create Pratica.java

---
+++ Java-course/src/Pratica.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Pratica {
+    public static void main(String[] args) {
+
+        Scanner ScNumeros = new Scanner(System.in);
+
+        System.out.println("Digite um numero");
+        int Numeropar= ScNumeros.nextInt();
+
+
+    }
+}
