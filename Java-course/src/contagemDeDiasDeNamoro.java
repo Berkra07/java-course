a9107ccd9d47ca5a8f277a71d201cfacbbb905d6
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-10-21 21:28:05 -0300

    Update contagemDeDiasDeNamoro.java

---
+++ Java-course/src/contagemDeDiasDeNamoro.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class contagemDeDiasDeNamoro {
+    public static void main(String[] args) {
+
+        System.out.println("A quantos dias estamos juntos");
+
+        Scanner ScAnosJuntos=new Scanner(System.in);
+
+        System.out.println("Digite quantos anos estão juntos: ");
+        int anosJuntos = ScAnosJuntos.nextInt();
+
+        
+    }
+}
