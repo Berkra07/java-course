bb83d2dd407507313ade652b6fb78836432eca05
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-10-27 21:04:24 -0300

    Create ControleRemoto.java

---
+++ POO-course/src/Aula06/ControleRemoto.java
@@ -0,0 +1,16 @@
+package Aula06;
+
+public class ControleRemoto {
+
+    private int volume;
+    private boolean ligado;
+    private boolean tocando;
+
+    public void Construtor(){
+        volume = 50;
+        ligado = false;
+        tocando=false;
+    }
+    
+}
+
