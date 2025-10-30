88bb044886a2459e0dd54970cc82532a28c59b93
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-10-29 21:40:02 -0300

    Criando o controle POO

--- POO-course/src/Aula06/ControleRemoto.java
+++ POO-course/src/Aula06/ControleRemoto.java
@@ -7,10 +7,33 @@ public class ControleRemoto {
     private boolean tocando;
 
     public void Construtor(){
-        volume = 50;
-        ligado = false;
-        tocando=false;
+        this.volume = 50;
+        this.ligado = false;
+        this.tocando=false;
     }
 
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public boolean getLigado() {
+        return ligado;
+    }
+
+    public void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+    public boolean getTocando() {
+        return tocando;
+    }
+
+    public void setTocando(boolean tocando) {
+        this.tocando = tocando;
+    }
 }
 
