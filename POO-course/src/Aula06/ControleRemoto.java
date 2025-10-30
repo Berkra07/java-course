18835eaa0fa68a9179b6b7720d3a5741b1a69513
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-10-29 21:47:03 -0300

    Update ControleRemoto.java

--- POO-course/src/Aula06/ControleRemoto.java
+++ POO-course/src/Aula06/ControleRemoto.java
@@ -1,6 +1,6 @@
 package Aula06;
 
-public class ControleRemoto {
+public class ControleRemoto implements Controlador{
 
     private int volume;
     private boolean ligado;
@@ -34,6 +34,51 @@ public class ControleRemoto {
 
     public void setTocando(boolean tocando) {
         this.tocando = tocando;
+    }
+
+    @Override
+    public void ligar() {
+
+    }
+
+    @Override
+    public void desligar() {
+
+    }
+
+    @Override
+    public void abrirMenu() {
+
+    }
+
+    @Override
+    public void fecharMenu() {
+
+    }
+
+    @Override
+    public void maisVolume() {
+
+    }
+
+    @Override
+    public void menosVolume() {
+
+    }
+
+    @Override
+    public void ligarMudo() {
+
+    }
+
+    @Override
+    public void play() {
+
+    }
+
+    @Override
+    public void pause() {
+
     }
 }
 
